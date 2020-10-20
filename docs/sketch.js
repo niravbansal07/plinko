49413adede3a9a902cd658eb86260bcde34370c6
niravbansal07 <69631330+niravbansal07@users.noreply.github.com>
2020-10-20 06:30:12 -0300

    Add files via upload

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,1 +1,24 @@
+var Engine = Matter.Engine;
+var World= Matter.World;
+var Bodies = Matter.Bodies;
+var Events = Matter.Events;
 
+var engine, world;
+var ground;
+
+function setup() {
+  createCanvas(480,800);
+  engine = Engine.create();
+   world = engine.world;
+
+
+    ground = new Ground(width/2,height,width,20);
+  
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine); 
+  ground.display();
+  
+}
